63a8e4df7e80d9790f039261f8dccc6c85ef0703
Carlos Marx <carlosmarx27@gmail.com>
2021-04-20 00:45:06 -0300

    Add validation to user profile edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :first_name, presence: true, length: {minimum:2, maximum:80}, on: :update
 
+  #Virtual Attribute
   def full_name
     [self.first_name, self.last_name].join(' ')
   end
