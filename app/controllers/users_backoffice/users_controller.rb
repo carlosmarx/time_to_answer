90ef813aa665dbe74b3c786c65799e3e1826e836
Carlos Marx <carlosmarx27@gmail.com>
2021-04-21 00:49:01 -0300

    Add active storage img profile user

--- app/controllers/users_backoffice/users_controller.rb
+++ app/controllers/users_backoffice/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersBackoffice::UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :build_profile
   layout 'users_backoffice'
+
+  private
+  
+    def build_profile
+      current_user.build_user_profile if current_user.user_profile.blank?
+    end
+    
 end
--- app/helpers/users_backoffice/users_helper.rb
+++ app/helpers/users_backoffice/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersBackoffice::UsersHelper
+  def avatar_url
+    avatar = current_user.user_profile.avatar
+    avatar.attached? ? avatar : "https://via.placeholder.com/150"
+  end
+  
 end
