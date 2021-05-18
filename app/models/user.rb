69307e6baaf121b59a58a31a8a40d6d2e3137252
Carlos Marx <carlosmarx27@gmail.com>
2021-05-18 02:17:05 -0300

    Add mailtrap config and recovery password feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   #Callback
   after_create :set_statistic
 
-  validates :first_name, presence: true, length: {minimum:2, maximum:80}, on: :update
+  validates :first_name, presence: true, length: {minimum:2, maximum:80}, on: :update, unless: :reset_password_token_present
          
   #Virtual Attribute
   def full_name
@@ -22,4 +22,9 @@ class User < ApplicationRecord
     def set_statistic
       AdminStatistic.set_event(AdminStatistic::EVENTS[:total_users])
     end
+
+    def reset_password_token_present
+      !!$global_params[:user][:reset_password_token]
+    end
+    
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,17 @@ Rails.application.configure do
   #Devise config
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  #Mailtrap
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => 'd9f5fd118daef6',
+    :password => 'c90ab21de57e56',
+    :address => 'smtp.mailtrap.io',
+    :domain => 'smtp.mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
